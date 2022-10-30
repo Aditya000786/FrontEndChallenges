7b3da17c35782847e6c006577d0321f542ca5568
Aditya Rai <raiaditya000786@gmail.com>
2022-10-30 18:28:04 +0530

    diff path

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ taskList.forEach((x) => {
   let li = document.createElement("li");
   let a = document.createElement("a");
   let image = document.createElement("img");
-  image.src = `/${x}/design/desktop-design.jpg`;
-  a.href = `/${x}`;
+  image.src = `/FrontEndChallenges/${x}/design/desktop-design.jpg`;
+  a.href = `/FrontEndChallenges/${x}/index.html`;
   a.innerText = x;
   li.appendChild(a);
   li.appendChild(image);
